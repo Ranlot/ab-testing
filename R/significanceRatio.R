bb1acac22a79a7228a4dc941ee85c8232e12774c
Ranlot <ranlot75@gmail.com>
2016-07-28 10:25:52 +0200

    setup for comparing different sample sizes

---
+++ R/significanceRatio.R
@@ -0,0 +1,24 @@
+testSignificanceRatio <- function(successRate, testRate) {
+
+  closedFunction <- function(numberOfEvents) {
+    
+  numbOfRepetitions <- 1e6
+  
+  makeTheTest <- makeGenericTest(successRate, testRate, numberOfEvents, F)
+  
+  one.Tailed.Result <- mclapply(1:numbOfRepetitions, makeTheTest, mc.cores = detectCores() - 1)
+  one.Tailed.Result <- do.call(rbind, one.Tailed.Result) %>% as.data.frame
+  
+  pValueHistogramPlotter(one.Tailed.Result, successRate, testRate, numberOfEvents)
+  
+  fractionOfSignificantRealizations <- sum(one.Tailed.Result$`theoretical p-value` < 0.05) / numbOfRepetitions
+  
+  cat(sprintf("successRate = %s\ttestRate = %s\tsample size = %d\tproportion of significant realizations = %.4f\n", successRate, testRate, numberOfEvents, fractionOfSignificantRealizations))
+  
+  c(successRate=successRate, testRate=testRate, numberOfEvents=numberOfEvents, fractionOfSignificantRealizations=fractionOfSignificantRealizations)
+  
+  }
+  
+  closedFunction
+  
+}
--- analysis.R
+++ analysis.R
@@ -7,6 +7,7 @@ source("R/significance.Wiki.R")
 source("R/empirical.pValue.Tester.R")
 source("R/Utils/plotter.Utils.R")
 source("R/Utils/dataSaver.Utils.R")
+source("R/significanceRatio.R")
 #------------------------------------------------------------------
 
 #how many events to we need in order to be able to say that at least 95% of the p-values are smaller than 0.05
@@ -17,23 +18,23 @@ source("R/Utils/dataSaver.Utils.R")
 #TODO: make a progress bar with pbapply package
 #https://gist.github.com/BERENZ/9236df77bfef83664305
 
-g(successRate, testRate, numberOfEvents) %=% g(0.14, 0.1, 400)  #define the parameters we wish to investigate
+g(successRate, testRate, numberOfEvents) %=% g(0.15, 0.1, 400)  #define the parameters we wish to investigate
 g(numberOfRepetitionsForPvals, numberOfRepetitionsForBootstrap, isPvalueComparison) %=% g(1e5, 4, F)  #configuration for number of elements in the averaging
 
 makeTheTest <- makeGenericTest(successRate, testRate, numberOfEvents, isPvalueComparison)  #get the significance testing function
-saveData <- genericSave(successRate, testRate, numberOfEvents, isPvalueComparison)
+
+#saveData <- genericSave(successRate, testRate, numberOfEvents, isPvalueComparison)
+#saveData(one.Tailed.Result)
 
 if (isPvalueComparison) {
   one.Tailed.Result <- replicate(numberOfRepetitionsForBootstrap, makeTheTest()) %>% t %>% as.data.frame
   #TODO: fix the xlim and ylim of the plot
   pValueComparator(theoreticalValues = one.Tailed.Result$`theoretical p-value`, empiricalValues = one.Tailed.Result$`empirical p-value`, numberOfRepetitionsForBootstrap)
 } else {
-  one.Tailed.Result <- mclapply(1:numberOfRepetitionsForPvals, makeTheTest, mc.cores = detectCores() - 1)
-  one.Tailed.Result <- do.call(rbind, one.Tailed.Result) %>% as.data.frame
+  testNumberOfEvents <- testSignificanceRatio(successRate, testRate)
+  
+  result <- lapply(seq(100, 1000, by = 100), testNumberOfEvents)
+  result <- do.call(rbind, result) %>% as.data.frame
 }
 
-saveData(one.Tailed.Result)
 
-pValueHistogramPlotter(one.Tailed.Result, successRate, testRate, numberOfEvents)
-
-sum(one.Tailed.Result$`theoretical p-value` < 0.05) / numberOfRepetitionsForPvals
