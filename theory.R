fafc6ce21d74ab321d496a5a9e0a2d661d51d8a2
Ranlot <ranlot75@gmail.com>
2016-08-08 11:12:11 +0200

    now with negative improvements

--- theory.R
+++ theory.R
@@ -30,9 +30,9 @@ makeOnePval <- function(successRate, testRate, N) {
   1 - one.prop.z.test(observedRate, testRate, N) %>% pnorm
 }
 
-g(testRate, numbOfReps) %=% g(0.1, 1e5)
+g(testRate, numbOfReps) %=% g(0.1, 1e6)
 
-successRates <- c(0.102, 0.105, 0.1075, 0.11, 0.115, 0.12)
+successRates <- c(0.096, 0.098, 0.1, 0.102, 0.105, 0.11, 0.12)
 #sampleSizes <- c(100, 500, 1000, 1500, 2000, 2500, 3000, 3500, 4000, 4500, 5000)
 sampleSizes <- sapply(logspace(log10(100), log10(10000), n=20), ceil)
 
@@ -76,9 +76,9 @@ convergenceSuccessRate <- function(x, testRate, dataSet) {
   xN <- logspace(log10(100), log10(10000), n=100)
   theoreticalValues <- sapply(xN, calcuteTheoreticalFraction, successRate=successRate, testRate=testRate)
   
-  plot(relevantData$N, relevantData$numericalResult, col=col, xaxt="n", yaxt="n", xlab="", ylab="", xlim=xLims, ylim=yLims, cex=2, pch=5, log="x")
+  plot(relevantData$N, relevantData$numericalResult, col=col, xaxt="n", yaxt="n", xlab="", ylab="", xlim=xLims, ylim=yLims, cex=2, pch=5, log="xy")
   par(new=T)
-  plot(xN, theoreticalValues, xaxt="n", yaxt="n", col=col, xlab="", ylab="", xlim=xLims, ylim=yLims, lwd=3, type="l", log="x")
+  plot(xN, theoreticalValues, xaxt="n", yaxt="n", col=col, xlab="", ylab="", xlim=xLims, ylim=yLims, lwd=3, type="l", log="xy")
   par(new=T)
   
 }
@@ -94,8 +94,8 @@ result <- do.call(rbind, result) %>% as.data.frame
 #--------------------------------------------------
 png(sprintf("convergencePlot_%s.png", testRate))
 #g(xLims, yLims) %=% g(c(-10, 5010), c(-5, 105))
-g(xLims, yLims) %=% g(c(100, 10000), c(5, 100))
-plot(1, type="n", xlab="Sample size", ylab="", xlim=xLims, ylim=yLims, main=sprintf("Probability of significant p-value\ntest rate p = %s", testRate), log="x", xaxt="n", yaxt="n")
+g(xLims, yLims) %=% g(c(100, 10000), c(0.5, 100))
+plot(1, type="n", xlab="Sample size", ylab="", xlim=xLims, ylim=yLims, main=sprintf("Probability of significant p-value\ntest rate p = %s", testRate), log="xy", xaxt="n", yaxt="n")
 par(new=T)
 
 
@@ -107,7 +107,7 @@ sideEffect <- sapply(whatToPlot, convergenceSuccessRate, dataSet=result, testRat
 
 magaxis(grid=T, frame.plot = F)
 legChars <- sapply(successRates, function(x) paste0(round(100 * (x - testRate) / testRate, 3), "%"))
-legend("topleft", legChars, col=plotColors, lwd=2, seg.len = 1, ncol=2)
+legend(x=95, y=1.9, legChars, col=plotColors, lwd=2, seg.len = 1, ncol=2)
 dev.off()
 
 #--------------------------------------------------
