7b4339d297c0ae18cd2241bd5ce2fc0ad6ea3b9b
Ranlot <ranlot75@gmail.com>
2016-08-11 10:31:07 +0200

    argument parsing

--- pValueComparator.R
+++ pValueComparator.R
@@ -3,13 +3,24 @@ library(parallel)
 library(pracma)
 library(latex2exp)
 library(magicaxis)
+library(optparse)
 source("R/Utils/smartEquals.R")
 source("R/significance.Wiki.R")
 source("R/empirical.pValue.Tester.R")
 source("R/Utils/plotter.Utils.R")
 #------------------------------------------------------------------
 
-g(successRate, testRate, numberOfEvents, averageSize) %=% g(0.102, 0.1, 50, 200)  #define the parameters we wish to investigate
+option_list <- list(
+  make_option("--successRate", type="numeric"),
+  make_option("--testRate", type="numeric"),
+  make_option("--numberOfEvents", type="numeric")
+)
+
+opt <- parse_args(OptionParser(option_list=option_list))
+
+g(successRate, testRate, numberOfEvents) %=% g(opt$successRate, opt$testRate, opt$numberOfEvents)
+averageSize <- 200
+#g(successRate, testRate, numberOfEvents, averageSize) %=% g(0.102, 0.1, 50, 200)
 
 makeTheTest <- makeGenericTest(successRate, testRate, numberOfEvents)  #get the significance testing function
 pValueComparatorPlotter <- genericPvalueComparator(successRate, testRate, numberOfEvents)
