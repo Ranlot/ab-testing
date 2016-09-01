ec432445473df84b6d7ae4ab1f4248813c31ce72
Laurent <ranlot75@gmail.com>
2016-09-01 09:07:10 +0300

    0th version of web app

---
+++ webApp/static/js/plotResult.js
@@ -0,0 +1,63 @@
+$(document).ready(function() {
+
+	$('result').empty();
+
+	$('#params_submit').click(function(event) {
+		event.preventDefault();
+		var pStar = $('input[name=pStar]').val();
+		var pBaseLine = $('input[name=pBaseLine]').val();
+		plotResult(pStar, pBaseLine);
+
+	});
+
+});
+
+var convData = {
+	element: 'plotConv',
+	xkey: 'x',
+	ykeys: ['y'],
+	labels: ['y'],
+	parseTime: false,
+	yLabelFormat: function (y) { return y.toFixed(1) + "%"; }
+};
+
+
+function plotResult(pStar, pBaseLine) {
+
+	var guaranteedRateChange = 100. * (pStar - pBaseLine) / pBaseLine
+
+	var queryResult = $.ajax({type:'POST', url:'/res', data:JSON.stringify({'pStar':pStar, 'pBaseLine':pBaseLine}), dataType:"json", contentType: "application/json"})
+
+	queryResult.done(function(data) {
+
+		//console.log(data);
+
+		var maxSampleSize = 5000;  //this value should actually be inherited from the backend....
+
+
+		var plotData = data['data']
+		var sampleSizeCut = data['sampleSizeCut']
+
+
+		console.log(sampleSizeCut);
+		console.log(maxSampleSize);
+		console.log(sampleSizeCut <= maxSampleSize);
+
+		console.log(pStar === pBaseLine)
+
+
+		if (pStar === pBaseLine) {
+			var text2print = "probability of getting significant p-value even though there is absolutely no difference"
+		} else if (pStar < pBaseLine)  {
+			var text2print = sampleSizeCut <= maxSampleSize ? "chance of getting significant p-value decreased to about 1% after " + sampleSizeCut + " events; This is despite the fact that we have a guaranteed decrease of " + guaranteedRateChange.toFixed(1) + "%" : "did not converge even after " + maxSampleSize + " events";
+		} else {
+			var text2print = sampleSizeCut <= maxSampleSize ? "number of iterations needed is " + sampleSizeCut + "change is " + guaranteedRateChange.toFixed(1) + "%" : "did not converge even after " + maxSampleSize + " events.  In fact you would need about " + sampleSizeCut + "events";
+		}
+
+		$('#result').text(text2print);
+
+		convData.data = plotData;
+		new Morris.Line(convData);
+
+		});
+}
--- webApp/webApp.py
+++ webApp/webApp.py
@@ -1,11 +1,113 @@
 from flask import Flask
+from flask import request
+from flask import render_template, Response, jsonify
+from scipy.integrate import quad
+from scipy.special import erfinv
+from scipy.optimize import fsolve
+from math import ceil
+import numpy as np
+from itertools import takewhile
 
 app = Flask(__name__)
 
+#!!! make sure it'll adapt to the relevant display !!!
+#!!! save user input and calculated result to key-value store
+
+#http://flask.theoryandpractice.org/?mux=3.&muy=3.7
+
+class Parameters:
+
+	significanPvalue = 0.95
+
+	def __init__(self, pStar, pBaseLine):
+        	self.pStar = pStar
+        	self.pBaseLine = pBaseLine
+
+	def isValid(self):
+		return True
+
+    	def convertInput(self):
+        	self.pStar = float(self.pStar)
+        	self.pBaseLine = float(self.pBaseLine)
+		#self.isValid()
+		#except:
+		#	return "not good"
+
+	def calculateN(self):
+		return self.pStar + self.pBaseLine
+
+	#define theoretical pdf with a closure
+	def _theoreticalPDF(self, sampleSize):
+		muZ = (self.pStar - self.pBaseLine) * np.sqrt(sampleSize / (self.pBaseLine * (1.0 - self.pBaseLine)))
+		sigZ = np.sqrt( (self.pStar * (1.0 - self.pStar)) / (self.pBaseLine * (1.0 - self.pBaseLine)) )
+		preFactor = np.exp( - muZ**2 / (2.0 * sigZ**2) ) / sigZ
+		def closedPDF(p):
+			return 100.0 * preFactor * np.exp((1.0 - 1.0 / sigZ**2) * erfinv(1 - 2*p)**2) * np.exp(-np.sqrt(2) * muZ / sigZ**2 * erfinv(1- 2*p))
+        	return closedPDF
+
+	def _integralCalculator(self, sampleSize):
+		integralResult = quad(self._theoreticalPDF(sampleSize), self.significanPvalue, 1.0)
+		return integralResult[0]
+
+	def _setUp(self, sampleSize):
+		return self._integralCalculator(sampleSize) - 95.0
+
+	def getResultSampleSize(self):
+		numbOfPlotPoints = 50
+		minSampleSize, maxSampleSize = 10, 5000
+
+		sampleSizes = (int(np.floor(sample)) for sample in np.linspace(minSampleSize, maxSampleSize, num=numbOfPlotPoints))
+		plotResults = ({'x':sample, 'y':self._integralCalculator(sample)} for sample in sampleSizes)
+
+		if self.pStar >= self.pBaseLine:
+			validResults = list(takewhile(lambda plotResult: np.isfinite(plotResult['y']) and plotResult['y'] <= 99.0, plotResults))
+		else:
+			validResults = list(takewhile(lambda plotResult: np.isfinite(plotResult['y']) and plotResult['y'] >= 1.0, plotResults))	
+
+		print validResults
+
+		numbOfNecessaryIterations = len(validResults)
+
+		if numbOfNecessaryIterations < numbOfPlotPoints:
+
+			if self.pStar >= self.pBaseLine:
+				sampleSizeCut = min(validResults, key=lambda x: np.abs(x['y']-95))['x']
+			else:
+				sampleSizeCut = min(validResults, key=lambda x: np.abs(x['y']-1))['x']
+	
+			sampleSizeCut = int(np.floor(sampleSizeCut))
+		else:
+			if self.pStar >= self.pBaseLine:
+				suggestedCut = fsolve(self._setUp, maxSampleSize)[0]
+				print suggestedCut
+				verifyCloseness = np.isclose(self._integralCalculator(suggestedCut), 95.0, atol=0.1)
+
+				if verifyCloseness:
+					sampleSizeCut = int(np.floor(suggestedCut))
+					sampleSizes = (int(np.floor(sample)) for sample in np.linspace(minSampleSize, sampleSizeCut, num=numbOfPlotPoints))
+					plotResults = ({'x':sample, 'y':self._integralCalculator(sample)} for sample in sampleSizes)
+					validResults = list(takewhile(lambda plotResult: np.isfinite(plotResult['y']) and plotResult['y'] <= 99.0, plotResults))
+				else:
+					sampleSizeCut = 2 * maxSampleSize
+
+			else:
+				sampleSizeCut = 2 * maxSampleSize
+
+		return {'data':validResults, 'sampleSizeCut':sampleSizeCut}
+
+@app.route('/res', methods=['POST'])
+def getResult():
+	paramsRequest = request.json
+	params = Parameters(paramsRequest['pStar'], paramsRequest['pBaseLine'])
+	params.convertInput()
+	return jsonify(params.getResultSampleSize())
+
 @app.route('/')
-def index():
-	return 'Prof. Linger is back... p-value convergence'
+def homePage():
+	return app.send_static_file('index.html')
+
 
 if __name__ == "__main__":
 	app.run()
+	#app.run(host='0.0.0.0', port=5556, debug=True)
 
